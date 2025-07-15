faae32022470ed74d70bb7073336fb095eeef207
Felipe Sardinha <ofelipe461@gmail.com>
2025-07-15 20:50:14 -0300

    criando pasta schemas e definindo schemas pydantic

---
+++ app/schemas/receita.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class ReceitaBase(BaseModel):
+    Nome: str 
+    Descricao: Optional[str] = None
+    Ingredientes: Optional[str] = None
+    TempoPreparo: Optional[int] = None
+    Categoria: Optional[str] = None
+    Origem: Optional[str] = None
+
+class ReceitaCreate(ReceitaBase):
+    pass
+
+class ReceitaRead(ReceitaBase):
+    ID: int
+    DataInclusao: datetime
+    DataEdicao: Optional[datetime]
+    DataExclusao: Optional[datetime]
+
+    class Config:
+        orm_mode = True
